7f9bd2d27114cc85105e8ea5d117cf0959387169
David Xu <xuqibo@ucla.edu>
2024-07-15 15:49:36 -0700

    s

--- main - new.py
+++ main - new.py
@@ -10,11 +10,14 @@ from Utils_preprocess import read_csv_info, fetch_map_model, normalize_raw_map
 from Utils_generate_dataset import data_to_npy
 
 
+temp_sample_path = "path_of_downloaded_temp_sample"  # we set a default path
+csv_path = "path_of_downloaded_csv_file"  # we set a default path
 
-csv_path = "path_of_downloaded_csv_file"
-output_dir = "path_of_output_dataset"
-query = "ribosome"
-fetch_bool = False
+output_dir = "path_of_output_dataset"  # user input for path of all datasets
+query = "ribosome"  # user input for EMDB search
+fetch_bool = False  # user input for RCSB search
+MODEL_PARTS = ["a", "b", "c", ...]  # user input as datastes' name
+
 
 def main(output_dir, csv_path):
 
@@ -23,12 +26,13 @@ def main(output_dir, csv_path):
     search_emdb(query, csv_path, fetch_classification=fetch_bool)
 
     # 1.2 Refine entries in the csv file
-    
+
 
 
     # Step 2. Download map and model files and do preprocessing
     # 2.1 Read map list and generate raw_map and model downloading paths
     csv_info, raw_map_paths, model_paths = read_csv_info(csv_path)
+    emdbs, pdbs, resolutions, emdb_ids = csv_info
 
     # 2.2 Download map and model files
     fetch_map_model(csv_info)
@@ -37,76 +41,72 @@ def main(output_dir, csv_path):
     map_paths = normalize_raw_map(csv_info, raw_map_paths)
 
     # Step 3. Generate a dataset (3d numpy arraies) from map and model files
-    # 3.1 Read model files and create lables
-    
-    # 3.2 Generate a dataset
-    if MAP2NPY:
+    # 3.1 Read map, model files and create lables
+    # idx of npy file
+    sample_num = 0
 
-        # idx of npy file
-        sample_num = 0
+    # num of tag in each part for all models
+    num_of_tag_in_each_part_for_all_models = {}
+    for part in MODEL_PARTS:
+        num_of_tag_in_each_part_for_all_models[part] = 0
 
-        # num of tag in each part for all models
-        num_of_tag_in_each_part_for_all_models = {}
-        for part in MODEL_PARTS:
-            num_of_tag_in_each_part_for_all_models[part] = 0
+    for idx, emdb_id in enumerate(emdb_ids):
+        # test for idx in [0,1]
+        # for idx, emdb_id in enumerate(emdb_ids[0:1]):
+        print(
+            f"[{idx+1}/{len(emdb_ids)}] Generating dataset from EMDB-{emdb_id}... "
+        )
+        sample_num, num_of_tag_in_each_part = data_to_npy(
+            map_paths[idx], model_paths[idx], MODEL_PARTS,
+            temp_sample_path, sample_num)
+        # Add number of each tag in new sampled model
+        for key, value_list in num_of_tag_in_each_part.items():
+            num_of_tag_in_each_part_for_all_models[key] += value_list
 
-        for idx, emdb_id in enumerate(emdb_ids):
-            # test for idx in [0,1]
-            # for idx, emdb_id in enumerate(emdb_ids[0:1]):
+    # Delete 0s in tag number list and calculate 1/ratio of tag number
+    ratio_of_tag = {}
+    for key, value_list in num_of_tag_in_each_part_for_all_models.items():
+        value_list = np.trim_zeros(value_list)
+        num_of_tag_in_each_part_for_all_models[key] = value_list
+        if 0 in value_list:
             print(
-                f"[{idx+1}/{len(emdb_ids)}] Generating dataset from EMDB-{emdb_id}... "
+                f'There is missing groups in part {key}, numbers of each class are {value_list}.'
             )
-            sample_num, num_of_tag_in_each_part = data_to_npy(
-                map_paths[idx], model_paths[idx], MODEL_PARTS,
-                temp_sample_path, sample_num)
-            # Add number of each tag in new sampled model
-            for key, value_list in num_of_tag_in_each_part.items():
-                num_of_tag_in_each_part_for_all_models[key] += value_list
+        ratio_of_tag[key] = [
+            int(value_list[0] // value_x) for value_x in value_list
+            if value_x != 0
+        ]
 
-        # Delete 0s in tag number list and calculate 1/ratio of tag number
-        ratio_of_tag = {}
-        for key, value_list in num_of_tag_in_each_part_for_all_models.items():
-            value_list = np.trim_zeros(value_list)
-            num_of_tag_in_each_part_for_all_models[key] = value_list
-            if 0 in value_list:
-                print(
-                    f'There is missing groups in part {key}, numbers of each class are {value_list}.'
-                )
-            ratio_of_tag[key] = [
-                int(value_list[0] // value_x) for value_x in value_list
-                if value_x != 0
-            ]
+    # Print statistical results
+    print(f"The number of .npy file: {sample_num}")
+    print("Num of each tag: ")
+    for key, value in num_of_tag_in_each_part_for_all_models.items():
+        print(f'{key}: {value}')
+    print("\nRatio of tags: ")
+    for key, value in ratio_of_tag.items():
+        print(f'{key}: {value}')
 
-        # Print statistical results
-        print(f"The number of .npy file: {sample_num}")
-        print("Num of each tag: ")
-        for key, value in num_of_tag_in_each_part_for_all_models.items():
-            print(f'{key}: {value}')
-        print("\nRatio of tags: ")
-        for key, value in ratio_of_tag.items():
-            print(f'{key}: {value}')
+    # 3.3 Split data into training and testing dataset
+    import splitfolders
+    # sample_path = os.path.join(output_dir, "TEST_with_blank", "train_val_data")
+    # sample_path = os.path.join(output_dir, "TEST_with_all", "train_val_data")
+    sample_path = os.path.join(output_dir, "TEST_20240520",
+                                "train_val_data")
 
-        # split data into training and testing dataset
-        import splitfolders
-        # sample_path = os.path.join(output_dir, "TEST_with_blank", "train_val_data")
-        # sample_path = os.path.join(output_dir, "TEST_with_all", "train_val_data")
-        sample_path = os.path.join(output_dir, "TEST_20240520",
-                                   "train_val_data")
+    os.makedirs(sample_path, exist_ok=True)
+    splitfolders.ratio(input=temp_sample_path,
+                        output=sample_path,
+                        seed=44,
+                        ratio=(.8, .2),
+                        group_prefix=None,
+                        move=True)
+    shutil.rmtree(temp_sample_path)
 
-        os.makedirs(sample_path, exist_ok=True)
-        splitfolders.ratio(input=temp_sample_path,
-                           output=sample_path,
-                           seed=44,
-                           ratio=(.8, .2),
-                           group_prefix=None,
-                           move=True)
-        shutil.rmtree(temp_sample_path)
-
-        # Calculate weight, create weight file and save it (ratio of tags)
-        weight_path = os.path.join(sample_path,
-                                   'class_weight_for_training.txt')
-        with open(weight_path, "w") as file:
-            json.dump(ratio_of_tag, file)
+    # 3.4 Calculate weight, create weight file and save it (ratio of tags)
+    weight_path = os.path.join(sample_path,
+                                'class_weight_for_training.txt')
+    with open(weight_path, "w") as file:
+        json.dump(ratio_of_tag, file)
 
 
 if __name__ == "__main__":
