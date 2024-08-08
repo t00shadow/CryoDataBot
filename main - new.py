83560879a3e74fd40bac639807b6125d5ec3cec0
David Xu <xuqibo@ucla.edu>
2024-08-08 16:25:01 -0700

    part1_2

--- main - new.py
+++ main - new.py
@@ -6,7 +6,7 @@ import numpy as np
 # import pandas as pd
 
 from backup.Utils_search_EMDB import search_emdb, refine_csv
-from Utils_preprocess import read_csv_info, fetch_map_model, normalize_raw_map
+from backup.Utils_preprocess import read_csv_info, fetch_map_model, normalize_raw_map
 from Utils_generate_dataset import data_to_npy, splitfolders
 
 
