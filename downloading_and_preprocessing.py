f1809f777c9297d8045c9330b630bd632d2e2a75
David Xu <xuqibo@ucla.edu>
2024-09-17 12:28:33 -0700

    comments

--- downloading_and_preprocessing.py
+++ downloading_and_preprocessing.py
@@ -21,6 +21,7 @@ logging.basicConfig(filename=raw_data_dir+'/download_and_preprocessing.log', enc
                     format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
+# main function
 def download_and_preprocessing(metadata_path, raw_dir, overwrite=False):
     """
     Reads metadata, downloads map and model files, and preprocesses the map files.
@@ -56,6 +57,7 @@ def download_and_preprocessing(metadata_path, raw_dir, overwrite=False):
     preprocess_maps(path_info)
 
 
+# Step1: create map and model paths for downloading and preprocessing from csv info
 def read_csv_info(csv_path, raw_dir):
     """
     Reads metadata from a CSV file and generates paths for downloading raw map and model files.
@@ -102,7 +104,36 @@ def read_csv_info(csv_path, raw_dir):
     return csv_info, path_info
 
 
-def download_one_map(emdb, pdb, emdb_id, raw_map_path, model_path, overwrite = False):
+# Step2: download maps and models using multithreasing
+def fetch_map_model(csv_info, path_info, overwrite=False):
+    """
+    Downloads map and model files concurrently using a thread pool.
+
+    Parameters:
+    csv_info (tuple): A tuple containing lists of EMDB identifiers, PDB identifiers, 
+                      and EMDB IDs extracted from the metadata CSV.
+    path_info (tuple): A tuple containing lists of raw map paths and model paths.
+    overwrite (bool): If True, existing files and directories will be overwritten.
+
+    Steps:
+    1. Extracts EMDB identifiers, PDB identifiers, and EMDB IDs from csv_info.
+    2. Extracts raw map paths and model paths from path_info.
+    3. Uses ThreadPoolExecutor to download map and model files concurrently.
+       - Submits download_one_map function for each map and model file.
+    4. Uses tqdm to display a progress bar for the download tasks.
+    """
+    emdbs, pdbs, _, emdb_ids = csv_info
+    raw_map_paths, model_paths = path_info
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(download_one_map, emdb, pdb, emdb_id, raw_map_path, model_path, overwrite)\
+                   for emdb, pdb, emdb_id, raw_map_path, model_path in zip(emdbs, pdbs, emdb_ids, raw_map_paths, model_paths)]
+        with logging_redirect_tqdm():
+            for _ in tqdm(as_completed(futures), total=len(emdbs), desc="Downloading map/pdb files"):
+                pass
+
+
+# Step2.1: download the map and model of one entry
+def download_one_map(emdb, pdb, emdb_id, raw_map_path, model_path, overwrite=False):
     """
     Downloads and extracts a map file from the Electron Microscopy Data Bank (EMDB) 
     and a model file from the Protein Data Bank (PDB).
@@ -173,31 +204,163 @@ def download_one_map(emdb, pdb, emdb_id, raw_map_path, model_path, overwrite = F
     return
 
 
-def fetch_map_model(csv_info, path_info, overwrite = False):
+# Step3: preprocess maps using multithreasing
+def preprocess_maps(path_info: list, give_map: bool=False, protein_tag_dist: int=1, map_threashold: float=0.01):
     """
-    Downloads map and model files concurrently using a thread pool.
+    Preprocesses multiple map files by normalizing them and calculating their fitness with models.
 
     Parameters:
-    csv_info (tuple): A tuple containing lists of EMDB identifiers, PDB identifiers, 
-                      and EMDB IDs extracted from the metadata CSV.
-    path_info (tuple): A tuple containing lists of raw map paths and model paths.
-    overwrite (bool): If True, existing files and directories will be overwritten.
+    path_info (list): A list containing map paths and CIF paths.
+    give_map (bool): If True, saves the normalized maps and binary maps.
+    protein_tag_dist (int): Theoretical atomic radii for map to model fitness calculation.
+    map_threashold (float): Normalized map density cutoff.
 
     Steps:
-    1. Extracts EMDB identifiers, PDB identifiers, and EMDB IDs from csv_info.
-    2. Extracts raw map paths and model paths from path_info.
-    3. Uses ThreadPoolExecutor to download map and model files concurrently.
-       - Submits download_one_map function for each map and model file.
-    4. Uses tqdm to display a progress bar for the download tasks.
+    1. Extracts map paths and CIF paths from path_info.
+    2. Logs the start of the preprocessing process.
+    3. Initializes an empty list to store results.
+    4. Uses tqdm to display a progress bar for the preprocessing tasks.
+       - Iterates through each map path and CIF path.
+       - Calls preprocess_one_map for each map and CIF file.
+       - Appends the result to the results list.
+    5. Logs the completion of the preprocessing process.
     """
-    emdbs, pdbs, _, emdb_ids = csv_info
-    raw_map_paths, model_paths = path_info
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(download_one_map, emdb, pdb, emdb_id, raw_map_path, model_path, overwrite)\
-                   for emdb, pdb, emdb_id, raw_map_path, model_path in zip(emdbs, pdbs, emdb_ids, raw_map_paths, model_paths)]
-        with logging_redirect_tqdm():
-            for _ in tqdm(as_completed(futures), total=len(emdbs), desc="Downloading map/pdb files"):
-                pass
+    map_paths, cif_paths = path_info
+
+    logging.info('-'*5+'Preprocessing Maps'+'-'*5)
+    print('-'*5+'Preprocessing Maps'+'-'*5)      
+    results = []
+    with logging_redirect_tqdm():
+        for map_path, cif_path in tqdm(zip(map_paths, cif_paths), total=len(map_paths), desc='Preprocessing Maps'):
+            result = preprocess_one_map(map_path, cif_path, give_map, protein_tag_dist, map_threashold)
+            results.append(result)
+    logging.info('-'*5+'Preprocessing Completed'+'-'*5)
+    print('-'*5+'Preprocessing Completed'+'-'*5)
+
+
+# Step3.1: preprocess the map of one entry
+def preprocess_one_map(map_path: str, cif_path: str, give_map: bool=False, protein_tag_dist: int=1, map_threashold: float=0.01):
+    """
+    Preprocesses a map file by normalizing it and calculating its fitness with a model.
+
+    Parameters:
+    map_path (str): Path to the map file.
+    cif_path (str): Path to the CIF file.
+    give_map (bool): If True, saves the normalized map and binary map.
+    protein_tag_dist (int): Theoretical atomic radii for map to model fitness calculation.
+    map_threashold (float): Normalized map density cutoff.
+
+    Returns:
+    tuple: 
+        vof (float): Volume overlap fraction (VOF) between the map and the model.
+        dice (float): Dice coefficient between the map and the model.
+
+    Steps:
+    1. Extracts protein and map IDs from the file paths.
+    2. Logs the preprocessing start information.
+    3. Loads and normalizes the map file using map_normalizing.
+       - Logs warnings and returns (0,0) if normalization fails.
+    4. Logs the successful loading of voxel data.
+    5. Calculates the map boundary.
+    6. Logs the map to model fitness calculation parameters.
+    7. Loads the CIF file using gemmi.read_structure.
+       - Logs warnings and returns (0,0) if reading fails.
+    8. Logs the successful loading of coordinate data.
+    9. Extracts and adjusts atom coordinates from the CIF file.
+    10. Logs the number of atoms in the CIF file.
+    11. Checks if any atom coordinates are out of bounds.
+        - Logs warnings and returns (0,0) if coordinates exceed map boundaries.
+    12. Creates a binary map for the protein coordinates.
+        - Rounds the coordinates to integers.
+        - Performs binary dilation to create spheres around atoms.
+    13. Applies the map threshold.
+    14. Calculates the overlap between the protein and the map.
+    15. Calculates the volume overlap fraction (VOF) and Dice coefficient.
+        - Logs warnings and returns (0,0) if calculation fails.
+    16. Logs the calculation completion information.
+    17. Saves the normalized map and binary map if give_map is True.
+    18. Returns the VOF and Dice coefficient.
+    """
+    protein_id = os.path.basename(cif_path).split(".")[0]
+    map_id = os.path.basename(map_path).split(".")[0]
+    save_path = os.path.dirname(map_path)
+
+    logging.info(f'Preprocessing Map:\n  FITTED_PDB: {protein_id} EMDB_ID: {map_id}')
+
+    # Load the map
+    try:
+        map_F, origin_info, _ = map_normalizing(map_path)
+    except ValueError as e:
+        logging.warning(f'  Error Normalizing Map: {e}\n  Preprocessing Failed')
+        return (0, 0)
+    else:
+        logging.info('  Successfully Loaded Voxel Data.')
+    map_boundary = np.shape(map_F)
+
+    logger.info(f'  Calculating Map to Model Fitness with Theoretical Atomic Radii as "{protein_tag_dist}" and Normalized Map Density Cutoff as "{map_threashold}"')
+    # Load the CIF file
+    try:
+        protein = gemmi.read_structure(cif_path)
+    except Exception as e:
+        logging.warning(f'  Error Reading CIF File: {e}\n  Preprocessing Failed')
+        return (0, 0)
+    else:
+        logging.info('  Successfully Loaded Coordinate Data')
+    protein_coords = np.array(atom_coord_cif(protein)).reshape(-1, 3)
+
+    # Adjust atom coordinates if origin is not (0,0,0)
+    protein_coords -= origin_info
+
+    logging.info(f'  Number of Atoms in CIF: {len(protein_coords)}')
+
+    # Check if any atom coordinates are out of bounds
+    try:
+        if np.any(np.any(protein_coords > map_boundary, axis=1)):
+            raise ValueError('  Out of bounds - atom coordinates exceed map boundaries')
+    except ValueError as e:
+        logging.warning(f'  Bound Error: {e}\n  Preprocessing Failed')
+        return (0, 0)
+
+    try:
+        # Create a binary map for the protein coordinates
+        protein_tag = cp.zeros(map_boundary, dtype=np.int8)
+
+        # Round the coordinates to integers
+        protein_coords = np.round(protein_coords).astype(int)
+        protein_tag[protein_coords[:, 0], protein_coords[:, 1], protein_coords[:, 2]] = 1
+
+        # Perform binary dilation to create spheres around atoms
+        structure = cp.ones((protein_tag_dist*2+1,) * 3, dtype=np.int8)
+        protein_tag = cp.asnumpy(binary_dilation(protein_tag, structure=structure).astype(np.int8))
+
+        # Apply the map threshold
+        map_F = np.where(map_F > map_threashold, 1, 0)
+
+        # Calculate overlap between the protein and the map
+        overlap = np.logical_and(protein_tag, map_F)
+        overlap_count = np.sum(overlap)
+
+        # Calculate the volume overlap fraction (VOF)
+        total_voxels_union = np.sum(np.logical_or(protein_tag, map_F))
+        vof = overlap_count / total_voxels_union
+
+        # Calculate Dice coefficient
+        dice = 2 * overlap_count / (np.sum(protein_tag) + np.sum(map_F))
+    except Exception as e:
+            logging.warning(f'  Error Calculating Map to Model Fitness: {e}\n  Preprocessing Failed')
+            return (0, 0)
+    else:
+        logging.info(f'  Calculation Completed:\n                           Volume overlap fraction (VOF): {(vof*100):.4f}% Dice Coefficient: {(dice*100):.4f}%')
+
+    if give_map:
+        with mrcfile.new(os.path.join(save_path, f'NORMALIZED_{map_id}.mrc'), overwrite=True) as mrc:
+            mrc.set_data(map_F)
+        logging.info(f'  Normalized Map Saved as "NORMALIZED_{map_id}.mrc"')
+        with mrcfile.new(os.path.join(save_path, f'CIF_{protein_id}.mrc'), overwrite=True) as mrc:
+            mrc.set_data(protein_tag)
+        logging.info(f'  Binary Map of {map_id} Saved as "BINARY_{map_id}.mrc"')
+
+    return (vof, dice)
 
 
 def map_normalizing(map_path):
@@ -274,162 +437,6 @@ def atom_coord_cif(structure):
                     coords.append((int(round(atom.pos.z)), int(round(atom.pos.y)), int(round(atom.pos.x))))
     return coords
 
-
-def preprocess_one_map(map_path: str, cif_path: str, give_map: bool=False, protein_tag_dist: int=1, map_threashold: float=0.01):
-    """
-    Preprocesses a map file by normalizing it and calculating its fitness with a model.
-
-    Parameters:
-    map_path (str): Path to the map file.
-    cif_path (str): Path to the CIF file.
-    give_map (bool): If True, saves the normalized map and binary map.
-    protein_tag_dist (int): Theoretical atomic radii for map to model fitness calculation.
-    map_threashold (float): Normalized map density cutoff.
-
-    Returns:
-    tuple: 
-        vof (float): Volume overlap fraction (VOF) between the map and the model.
-        dice (float): Dice coefficient between the map and the model.
-
-    Steps:
-    1. Extracts protein and map IDs from the file paths.
-    2. Logs the preprocessing start information.
-    3. Loads and normalizes the map file using map_normalizing.
-       - Logs warnings and returns (0,0) if normalization fails.
-    4. Logs the successful loading of voxel data.
-    5. Calculates the map boundary.
-    6. Logs the map to model fitness calculation parameters.
-    7. Loads the CIF file using gemmi.read_structure.
-       - Logs warnings and returns (0,0) if reading fails.
-    8. Logs the successful loading of coordinate data.
-    9. Extracts and adjusts atom coordinates from the CIF file.
-    10. Logs the number of atoms in the CIF file.
-    11. Checks if any atom coordinates are out of bounds.
-        - Logs warnings and returns (0,0) if coordinates exceed map boundaries.
-    12. Creates a binary map for the protein coordinates.
-        - Rounds the coordinates to integers.
-        - Performs binary dilation to create spheres around atoms.
-    13. Applies the map threshold.
-    14. Calculates the overlap between the protein and the map.
-    15. Calculates the volume overlap fraction (VOF) and Dice coefficient.
-        - Logs warnings and returns (0,0) if calculation fails.
-    16. Logs the calculation completion information.
-    17. Saves the normalized map and binary map if give_map is True.
-    18. Returns the VOF and Dice coefficient.
-    """
-    protein_id = os.path.basename(cif_path).split(".")[0]
-    map_id = os.path.basename(map_path).split(".")[0]
-    save_path = os.path.dirname(map_path)
-    
-    logging.info(f'Preprocessing Map:\n  FITTED_PDB: {protein_id} EMDB_ID: {map_id}')
-    
-    # Load the map
-    try:
-        map_F, origin_info, _ = map_normalizing(map_path)
-    except ValueError as e:
-        logging.warning(f'  Error Normalizing Map: {e}\n  Preprocessing Failed')
-        return (0,0)
-    else:
-        logging.info(f'  Successfully Loaded Voxel Data.')
-    map_boundary = np.shape(map_F)
-    
-    logger.info(f'  Calculating Map to Model Fitness with Theoretical Atomic Radii as "{protein_tag_dist}" and Normalized Map Density Cutoff as "{map_threashold}"')
-    # Load the CIF file
-    try:
-        protein = gemmi.read_structure(cif_path)
-    except Exception as e:
-        logging.warning(f'  Error Reading CIF File: {e}\n  Preprocessing Failed')
-        return (0,0)
-    else:
-        logging.info(f'  Successfully Loaded Coordinate Data')
-    protein_coords = np.array(atom_coord_cif(protein)).reshape(-1, 3)
-    
-    # Adjust atom coordinates if origin is not (0,0,0)
-    protein_coords -= origin_info
-    
-    logging.info(f'  Number of Atoms in CIF: {len(protein_coords)}')
-    
-    # Check if any atom coordinates are out of bounds
-    try:
-        if np.any(np.any(protein_coords > map_boundary, axis=1)):
-            raise ValueError('  Out of bounds - atom coordinates exceed map boundaries')
-    except ValueError as e:
-        logging.warning(f'  Bound Error: {e}\n  Preprocessing Failed')
-        return (0,0)
-    
-    try:
-        # Create a binary map for the protein coordinates
-        protein_tag = cp.zeros(map_boundary, dtype=np.int8)
-        
-        # Round the coordinates to integers
-        protein_coords = np.round(protein_coords).astype(int)
-        protein_tag[protein_coords[:, 0], protein_coords[:, 1], protein_coords[:, 2]] = 1
-        
-        # Perform binary dilation to create spheres around atoms
-        structure = cp.ones((protein_tag_dist*2+1,) * 3, dtype=np.int8)
-        protein_tag = cp.asnumpy(binary_dilation(protein_tag, structure=structure).astype(np.int8))
-
-        # Apply the map threshold
-        map_F = np.where(map_F > map_threashold, 1, 0)
-
-        # Calculate overlap between the protein and the map
-        overlap = np.logical_and(protein_tag, map_F)
-        overlap_count = np.sum(overlap)
-        
-        # Calculate the volume overlap fraction (VOF)
-        total_voxels_union = np.sum(np.logical_or(protein_tag, map_F))
-        vof = overlap_count / total_voxels_union
-        
-        # Calculate Dice coefficient
-        dice = 2 * overlap_count / (np.sum(protein_tag) + np.sum(map_F))
-    except Exception as e:
-            logging.warning(f'  Error Calculating Map to Model Fitness: {e}\n  Preprocessing Failed')
-            return (0,0)
-    else:
-        logging.info(f'  Calculation Completed:\n                           Volume overlap fraction (VOF): {(vof*100):.4f}% Dice Coefficient: {(dice*100):.4f}%')
-
-    if give_map:
-        with mrcfile.new(os.path.join(save_path, f'NORMALIZED_{map_id}.mrc'), overwrite=True) as mrc:
-            mrc.set_data(map_F)
-        logging.info(f'  Normalized Map Saved as "NORMALIZED_{map_id}.mrc"')
-        with mrcfile.new(os.path.join(save_path, f'CIF_{protein_id}.mrc'), overwrite=True) as mrc:
-            mrc.set_data(protein_tag)
-        logging.info(f'  Binary Map of {map_id} Saved as "BINARY_{map_id}.mrc"')
-    
-    return (vof, dice)
-
-
-def preprocess_maps(path_info: list, give_map: bool=False, protein_tag_dist: int=1, map_threashold: float=0.01):
-    """
-    Preprocesses multiple map files by normalizing them and calculating their fitness with models.
-
-    Parameters:
-    path_info (list): A list containing map paths and CIF paths.
-    give_map (bool): If True, saves the normalized maps and binary maps.
-    protein_tag_dist (int): Theoretical atomic radii for map to model fitness calculation.
-    map_threashold (float): Normalized map density cutoff.
-
-    Steps:
-    1. Extracts map paths and CIF paths from path_info.
-    2. Logs the start of the preprocessing process.
-    3. Initializes an empty list to store results.
-    4. Uses tqdm to display a progress bar for the preprocessing tasks.
-       - Iterates through each map path and CIF path.
-       - Calls preprocess_one_map for each map and CIF file.
-       - Appends the result to the results list.
-    5. Logs the completion of the preprocessing process.
-    """
-    map_paths, cif_paths = path_info
-
-    logging.info('-'*5+'Preprocessing Maps'+'-'*5)
-    print('-'*5+'Preprocessing Maps'+'-'*5)      
-    results = []
-    with logging_redirect_tqdm():
-        for map_path, cif_path in tqdm(zip(map_paths, cif_paths), total=len(map_paths), desc='Preprocessing Maps'):
-            result = preprocess_one_map(map_path, cif_path, give_map, protein_tag_dist, map_threashold)
-            results.append(result)
-    logging.info('-'*5+'Preprocessing Completed'+'-'*5)
-    print('-'*5+'Preprocessing Completed'+'-'*5)
 
 if __name__ == '__main__':
     matadata_path = 'Metadata/ribosome_res_1-4/ribosome_res_1-4.csv'
