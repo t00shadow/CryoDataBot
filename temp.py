a04064db3c96821a5be19ac219b51d21d9e6cae0
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2025-02-12 17:44:14 -0800

    fixed bug: empty metadata files no longer crash app

--- temp.py
+++ temp.py
@@ -15,3 +15,32 @@ hiddenimports = collect_submodules('cupy_backends.cuda')  # collect submodules o
 # hiddenimports += collect_submodules('cupy_backends.cuda')  # collect submodules only for CUDA backend
 #hiddenimports += collect_submodules('cupy_backends')  # collect submodules for all backends
 print(hiddenimports)
+
+
+import csv
+
+# def has_data_rows(csv_file):
+#     with open(csv_file, newline='', encoding='utf-8') as f:
+#         reader = csv.reader(f)
+#         rows = list(reader)  # Read all rows into a list
+#         return len(rows) > 1  # True if there is more than just the header
+
+def has_data_rows(csv_file):
+    with open(csv_file, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        _ = next(reader, None)  # Read the header row
+        return next(reader, None) is not None  # Try to read one more row
+
+# Example usage
+# csv_file = "JUNKSTUFF/CryoDataBot/download_file_034/download_file_034.csv"
+csv_file = "JUNKSTUFF/CryoDataBot/download_file_041/download_file_041.csv"
+if not has_data_rows(csv_file):
+    print("Invalid CSV: No data rows found.")
+else:
+    print("Valid CSV: Contains at least one data row.")
+
+
+
+import keyword
+print(keyword.iskeyword('for'))
+print(keyword.iskeyword('file_utils'))
