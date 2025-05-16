0b933575154242d2549387f101617fbf23556182
Leon <noel.uw.321@gmail.com>
2025-05-16 09:05:35 -0700

    Replace contents of main with temp branch snapshot

--- cryodatabot/__main__.py
+++ cryodatabot/__main__.py
@@ -9,7 +9,7 @@ from PyQt5 import QtGui as qtg
 from PyQt5.QtCore import QTextCodec
 codec = QTextCodec.codecForName("UTF-8")
 
-from guiskin_DEV import Ui_MainWindow    # need the "Ui_" prefix
+from guiskin import Ui_MainWindow    # need the "Ui_" prefix
 #from guiskin import Ui_MainWindow
 
 
