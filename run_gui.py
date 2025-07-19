329f6736dd1e1fe7a1c598e12855996c88369e3d
David Xu <xuqibo@ucla.edu>
2025-07-18 17:10:11 -0700

    test

--- run_gui.py
+++ run_gui.py
@@ -2,7 +2,7 @@
 
 
 # from cryodatabot.src.backend.core.redundancy_filter import main
-# from cryodatabot.src.backend.core.downloading_and_preprocessing_NO_GPU2 import main
+from cryodatabot.src.backend.core.downloading_and_preprocessing_NO_GPU2 import main
 from cryodatabot.src.backend.core.generate_dataset import main
 
 if __name__ == "__main__":
